b40973282717435bf34457fb3ea6c4448db29a68
Ömer Faruk Sayar <96315732+ofsayar@users.noreply.github.com>
2023-03-24 12:39:01 +0300

    Add files via upload

---
+++ Test/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=5,b=3;
+    printf("%d\n",a+b);
+    printf("Hello world!\n");
+    return 0;
+}
---
+++ Test/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h> //for bool data type
+int main(){
+
+    float jason=23.333;
+    double jason2=55.5555555555555e+11;
+    _Bool boolVariable=true; //it is valid for C89
+    bool abc=true;  //it is valid for C99
+    int rgbColor=0xFFEF0D; //we can use hexadecimal
+    printf("%d",rgbColor);
+
+
+
+    return 0;
+}
